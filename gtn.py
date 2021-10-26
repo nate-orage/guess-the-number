a451e9b3fdae51c32007059600e971d9e51106f0
nate-orage <nate54o@icloud.com>
2021-10-26 17:31:03 +0000

    Added a function to clear the screen. Added comments. Fixed correct_num to choose the correct number at random.

--- gtn.py
+++ gtn.py
@@ -1,6 +1,7 @@
 #!/home/nate/python/python_env/bin/python3
 """Guess the Number"""
 import random
+from os import system, name
 
 
 def mode_check(list):
@@ -10,13 +11,16 @@ def mode_check(list):
     )
     easy = ["easy", "Easy"]
     hard = ["hard", "Hard"]
+    mode_choice = f"\nYou have selected {mode} mode.\n Good luck, {name}!!\n"
+    # Generate list for Easy Mode
     if mode in easy:
         list = [random.randint(1, 30) for i in range(10)]
-        print(f"\nYou have selected {mode} mode.\n Good luck!!\n")
+        print(mode_choice)
         return list
+    # Generate list for Hard Mode
     elif mode in hard:
         list = [random.randint(1, 40) for i in range(20)]
-        print(f"\nYou have selected {mode} mode.\n Good luck!!\n")
+        print(mode_choice)
         return list
     else:
         return mode_check(list)
@@ -33,14 +37,15 @@ def guess_the_number():
 def easy_counter():
     # This function outputs the list of numbers to choose from and tells you how many turns you have left based on a counter.
     global counter
+    # This chooses the winning number based on a random selection of an index position.
+    correct_num = list[(random.randint(0, 9))]
+    clear()
     while counter < 11:
-        # I decided that the correct number will be in the same position every time. However the list will shuffle each time.
-        correct_num = list[-5]
         winner = f"\nYou win! {correct_num} is the correct number!"
         incorrect = f"\nIncorrect. Please try again. You have {10 - counter} attempts remaining.\n"
         lose = "You lose. Goodbye"
         guess = input(
-            f"Take a guess!\nYou have a total of {len(list)} numbers to choose from.\n\nYour choices are:\n{list}.\n"
+            f"Take a guess!\nYou have a total of {len(list)} numbers to choose from.\n\nYour choices are:\n{list}\n"
         )
         try:
             # This verifies that the input is a whole number and not a float or str.
@@ -51,15 +56,17 @@ def easy_counter():
             if counter == 10:
                 print(lose)
                 break
-            continue
+            # continue
         guess = int(guess)
         if guess is correct_num:
             print(winner)
             break
         if counter == 10:
+            clear()
             print(lose)
             break
         elif guess != correct_num:
+            clear()
             counter += 1
             print(incorrect)
 
@@ -67,16 +74,15 @@ def easy_counter():
 def hard_counter():
     # This function outputs the list of numbers to choose from and tells you how many turns you have left based on a counter.
     global counter
+    # This chooses the winning number based on a random selection of an index position.
+    correct_num = list[(random.randint(0, 19))]
+    clear()
     while counter < 21:
-        # I decided that the correct number will be in the same position every time. However the list will shuffle each time.
-        correct_num = list[-10]
-        print(list)
-        print(correct_num)
         winner = f"\nYou win! {correct_num} is the correct number!"
         incorrect = f"\nIncorrect. Please try again. You have {20 - counter} attempts remaining.\n"
         lose = "You lose. Goodbye"
         guess = input(
-            f"Take a guess!\nYou have a total of {len(list)} numbers to choose from.\n\nYour choices are:\n{list}.\n"
+            f"Take a guess!\nYou have a total of {len(list)} numbers to choose from.\n\nYour choices are:\n{list}\n"
         )
         try:
             int(guess)
@@ -86,20 +92,31 @@ def hard_counter():
             if counter == 20:
                 print(lose)
                 break
-            continue
         guess = int(guess)
         if guess is correct_num:
             print(winner)
             break
         if counter == 20:
+            clear()
             print(lose)
             break
         elif guess != correct_num:
+            clear()
             counter += 1
             print(incorrect)
 
 
+def clear():
+    # for windows
+    if name == "nt":
+        _ = system("cls")
+
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system("clear")
+
+
 name = input(f"\nHello, what" "s your name?\n")
-list = mode_check(list)
 counter = 0
+list = mode_check(list)
 guess_the_number()
